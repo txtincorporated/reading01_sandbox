1f9eca4b3566497afadc56cb61cdf06cadddf874
Nathan P. Keene <npk@txtincorporated.com>
2016-06-20 01:04:38 -0700

    Problem 4

--- problem4/script.js
+++ problem4/script.js
@@ -1,4 +1,14 @@
 //PROBLEM 4:
 $('#reveal').on('click', function() {
-  $('#cover').attr('id','cover2');
+  $('#cover').removeAttr('id');
+  $('#reveal').attr('id', 'restore');
+  $('#restore').text('RESTORE');
+  $('#restore').on('click', function() {
+    $('body div:nth-child(2)').attr('id', 'cover');
+    $('#restore').attr('id','reveal');
+    $('#reveal').text('RELOAD');
+    $('#reveal').on('click', function() {
+      location.reload();
+    });
+  });
 });
